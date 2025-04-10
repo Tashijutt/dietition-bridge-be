4104ff52d070035d786e6bf306b140f1e97eca0e
usman <usman@carswitch.com>
2025-04-10 12:29:16 +0500

    New fields added for user

--- models/User.js
+++ models/User.js
@@ -4,7 +4,15 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ['user', 'dietitian', 'admin'], default: 'user' }
+  role: { type: String, enum: ['user', 'dietitian', 'admin'], default: 'user' },
+  profilePicture: { type: String, default: '' },
+  phone: { type: String, default: '' },
+  gender: { type: String, enum: ['male', 'female', 'other'], default: 'other' },
+  age: { type: Number, default: 0 },
+  weight: { type: Number, default: 0 },
+  height: { type: Number, default: 0 },
+  healthConditions: { type: [String], default: [] },
+  dietaryPreferences: { type: String, default: '' },
 }, { timestamps: true });
 
 export default mongoose.model('User', userSchema);
